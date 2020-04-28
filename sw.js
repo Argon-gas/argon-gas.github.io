090f78e9e6df6b2c82e30d85da1760cfd9f260e0
Argon-gas <sunzhicheng2003@outlook.com>
2020-04-28 15:40:14 +0800

    Site updated: 2020-04-28 15:40:14

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","79e410414f77f6abf4be0c5d24a1f3b0"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
